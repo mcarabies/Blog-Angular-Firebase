db835257e9397abeb7cbbcdd14c2075bf304e5da
Marcelo C <marcelojcarabies@gmail.com>
2020-01-18 21:01:43 -0300

    segundo commit pre desarrollo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'posts', loadChildren: () => import('./components/posts/list-posts/list-posts.module').then(m => m.ListPostsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
