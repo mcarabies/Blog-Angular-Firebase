eead1a4618f1ba831f7e8f05307502a11bd7614b
Marcelo C <marcelojcarabies@gmail.com>
2020-01-19 21:55:52 -0300

    pagina de post lista

--- src/app/components/posts/post/post.component.ts
+++ src/app/components/posts/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from "@angular/router";
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  public post: {
+    id: string;
+    titlePost: string;
+    contentPost: string;
+    imagePost: string;
+  } = {
+    id: '1',
+    titlePost: 'Post One',
+    contentPost: 'Hola Mundo',
+    imagePost: 'https://i.picsum.photos/id/237/200/300.jpg'
+  }
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.post.id = this.route.snapshot.params.id;
   }
 
 }
